9ac8eae1b566937f879b6446947e1d567e06c55a
jurialmunkey <mergeandvary@gmail.com>
2017-03-19 22:15:06 +1100

    verifications

--- extras/verify.py
+++ extras/verify.py
@@ -5,16 +5,13 @@ import os.path
 dialog      = xbmcgui.Dialog()
 
 VERIFY = '/system/priv-app/NetworkTest/NetworkTest.apk'
-ALTVERIFY = '~/verify'
 
-if os.path.isfile(VERIFY) or os.path.isfile(ALTVERIFY):
+if os.path.isfile(VERIFY):
     xbmc.executebuiltin('Skin.SetBool(Verification)')
     xbmc.executebuiltin('PlayMedia(special://skin/tvpmc/videos/tvpmcintro.mp4)')
     if xbmc.getCondVisibility("Skin.HasSetting(TVPLUS.Welcome.Accepted)"):
         xbmc.executebuiltin('ReplaceWindow(home)')
     else:
         xbmc.executebuiltin('ReplaceWindow(1133)')
-    
 else:
-    dialog.ok('TVPMC Verification','TVPMC Verification Failed!')
-    xbmc.executebuiltin('Quit')
+    xbmc.executebuiltin('ActivateWindow(1156)')
